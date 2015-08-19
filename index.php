4b1ca37e05488bc60f32f00f2f04712502814110
Tony James <tony.james@cohaesus.co.uk>
2015-08-19 13:04:48 +0100

    Added index file

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>   
+<head>
+	<meta charset="utf-8">
+	<!--[if IE]><![endif]-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<!-- !CSS -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<!-- Uncomment if you are specifically targeting less enabled mobile browsers
+	<link rel="stylesheet" media="handheld" href="css/handheld.css?v=1">  -->	
+</head>
+<body>
+<div class="row-fluid">
+	<div class="span12">
+		<div class="row-fluid">
+			<div class="span3"></div>
+			<div class="span6">
+				<div class="panel panel-default">
+					<div class="panel-heading"><h4>Add a blog post</h4></div>
+					<div class="panel-body">
+						<p class="lead">This is a demo blog for me.</p>
+						<form action="index.php" method="" accept-charset="utf-8">
+							<label for="author">Author: </label><input id="author" type="text" name="author" value="">
+							<label for="post">Post: </label><input id="post" type="text" name="post" value="">
+							<button type="button" class="btn btn-primary">Post This</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="span3"></div>
+		</div>
+	</div>
+</div>
+</body>
